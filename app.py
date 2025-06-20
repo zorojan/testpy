dbfeb4e7c87af78f857305c83a6a87c9d82eefc1
zorojan <zohrap.vardanyan@gmail.com>
2025-06-20 18:05:16 +0400

    Create app.py
    Add initial app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="Моё первое приложение Zorojan")
+
+st.title("Привет от Streamlit, Zorojan!")
+st.write("Это простое приложение, развернутое из GitHub.")
+
+user_name = st.text_input("Как вас зовут?")
+if user_name:
+    st.write(f"Приятно познакомиться, {user_name}!")
+
+favorite_number = st.slider("Выберите любимое число", 0, 100, 50)
+st.write(f"Ваше любимое число: {favorite_number}")
